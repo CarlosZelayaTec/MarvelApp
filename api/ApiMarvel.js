e1e4066fa1cdec59dea56087a81c8ade9dbc8341
Carlos <czelayabaca@gmail.com>
2022-08-30 12:28:25 -0600

    Fix: Resuelto algunos errores que nos daba el .env

--- api/ApiMarvel.js
+++ api/ApiMarvel.js
@@ -1,8 +1,5 @@
-import {config as dotenv} from 'dotenv';
-dotenv();
-
-const apikey = process.env.APIKEY;
-const hash = process.env.HASH;
+const apikey = 'a607ad46e955867cb0306b83d30a2634';
+const hash = 'f60caff63104de71b8edcf38ab49de9e';
 
 export async function getComics(){
     const response = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=1000&apikey=${apikey}&hash=${hash}`);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,27 +3,24 @@ import React, { useState, useEffect } from 'react'
 
 const HomeScreen = () => {
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  const fetchApi = async () =>{
-    // const response = await fetch('http://gateway.marvel.com/v1/public/comics?apikey=a607ad46e955867cb0306b83d30a2634');
-    const response = await fetch('http://gateway.marvel.com/v1/public/comics?ts=1000&apikey=a607ad46e955867cb0306b83d30a2634&hash=f60caff63104de71b8edcf38ab49de9e');
-    const {data} = await response.json();
-    const {results} = await data;
-    // console.log(results);
-    setData(results);
-  }
+  // const fetchApi = async () =>{
+  //   // const response = await fetch('http://gateway.marvel.com/v1/public/comics?apikey=a607ad46e955867cb0306b83d30a2634');
+  //   const response = await fetch('http://gateway.marvel.com/v1/public/comics?ts=1000&apikey=a607ad46e955867cb0306b83d30a2634&hash=f60caff63104de71b8edcf38ab49de9e');
+  //   const {data} = await response.json();
+  //   const {results} = await data;
+  //   // console.log(results);
+  //   setData(results);
+  // }
 
-  useEffect(() => {
-    fetchApi();
-  }, [])
+  // useEffect(() => {
+  //   fetchApi();
+  // }, [])
 
   return (
     <View>
-      {
-        data.map((x) => <Text>{x.title}</Text>)
-      }
-      {/* <Text>HomeScreen</Text> */}
+      <Text>HomeScreen</Text>
     </View>
   )
 }
